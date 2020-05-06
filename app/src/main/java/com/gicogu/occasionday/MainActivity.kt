ef9236bea93c8587bf80d06fb95d3c341f794aaf
Juan G. Coronado <jucoronado@uan.edu.co>
2020-05-05 21:58:23 -0500

    Reservados

--- app/src/main/java/com/gicogu/occasionday/MainActivity.kt
+++ app/src/main/java/com/gicogu/occasionday/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.gicogu.occasionday
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.RadioGroup
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
+
+
+    fun registrousu(vista: View){
+        val intent = Intent( this@MainActivity, Registrar::class.java)
+        startActivity(intent)
+    }
+
+    fun restablecer(vista: View){
+
+        val intent = Intent( this@MainActivity, Restablecer::class.java)
+        startActivity(intent)
+
+    }
+
+
 }
---
+++ app/src/main/java/com/gicogu/occasionday/Registrar.kt
@@ -0,0 +1,127 @@
+package com.gicogu.occasionday
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_registrar.view.*
+
+
+class Registrar : AppCompatActivity() {
+    private lateinit var getcorreo: EditText
+    private lateinit var getnombre: EditText
+    private lateinit var  getapellido: EditText
+    private lateinit var getpass: EditText
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDatabase: FirebaseDatabase
+    private lateinit var mreferencia: DatabaseReference
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar)
+        getcorreo= findViewById(R.id.correoreg)
+        getnombre= findViewById(R.id.nombreusu)
+        getapellido= findViewById(R.id.apelusu)
+        getpass= findViewById(R.id.clave)
+        mAuth = FirebaseAuth.getInstance()
+        mDatabase = FirebaseDatabase.getInstance()
+        mreferencia= mDatabase.reference.child("Usuarios")
+    }
+
+
+
+    fun registro(vista: View){
+        val correo:String = getcorreo.text.toString()
+        val nombre:String = getnombre.text.toString()
+        val apellido:String = getapellido.text.toString()
+        val pass:String = getpass.text.toString()
+
+        var grupo = findViewById(R.id.grupo) as RadioGroup
+        var a=""
+        if(grupo.getCheckedRadioButtonId() == R.id.clienter){
+
+            a = "Clientes"
+        }else if (grupo.getCheckedRadioButtonId() == R.id.socior){
+            a = "Socios"
+        }else {Toast.makeText(applicationContext, "Que Diantres", Toast.LENGTH_SHORT).show()}
+
+
+
+
+        if (!correo.isEmpty() && !nombre.isEmpty() && !apellido.isEmpty() && !pass.isEmpty())
+        {
+            if(pass.length >=6 ){
+
+        /*        grupo.setOnCheckedChangeListener({ group, checkedId ->
+                    val rb = findViewById<RadioButton>(checkedId)
+                    var a=""
+                    when (rb) {
+
+                        rb.clienter -> fireusuario(correo, nombre, apellido ,pass,"Clientes")
+                        rb.socior -> fireusuario(correo, nombre, apellido ,pass,"Socios")
+                    } }   )*/
+                fireusuario(correo, nombre, apellido ,pass,a)
+
+            }else{
+                Toast.makeText(applicationContext, "La contraseña debe tener más de 5 digitos", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            Toast.makeText(applicationContext, "Debe completar los campos", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun fireusuario(correo: String, nombre: String, apellido: String, pass:String, usuario:String){
+        val usu=usuario
+        mAuth.createUserWithEmailAndPassword(correo,pass).addOnCompleteListener(this){
+                task ->
+
+            if(task.isComplete){
+                val user:FirebaseUser?=mAuth.currentUser
+                mailverify(user)
+
+                val userBD=mreferencia.child(usu).child(user?.uid.toString())
+                userBD.child("nombre").setValue(nombre)
+                userBD.child("apellido").setValue(apellido)
+                userBD.child("correo").setValue(correo)
+                logIn()
+            }else {
+                Toast.makeText(applicationContext, "No se pudeo registrar usuario", Toast.LENGTH_SHORT).show()
+            }
+        }   }
+
+    private fun mailverify(user:FirebaseUser?){
+
+        user?.sendEmailVerification()?.addOnCompleteListener(this){ task->
+            if(task.isComplete){
+                Toast.makeText(applicationContext, "Correo Registrado", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(applicationContext, "Correo no registradoo", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun logIn(){
+        Toast.makeText(applicationContext, "Gracias", Toast.LENGTH_SHORT).show()
+        val intent = Intent( this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    fun cancelar(vista: View){
+
+        val intent = Intent( this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+}
